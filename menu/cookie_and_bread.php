f0259364a894bfc02d220165ff51444b00182042
Lee Hak Jun <harleybird23@gmail.com>
2022-06-02 22:38:19 +0900

    cookie menu update

--- menu/cookie_and_bread.php
+++ menu/cookie_and_bread.php
@@ -16,33 +16,25 @@
     <section id="menu-section">
 		<div style="text-align: center;">
 			<img src="/ODW/ODW-Private/menus/cookies.jpg" alt="01" class="menu-main-img"/>
-		</div>	
+		</div>
 		<h1>이것이 바로오♡ 꾸끼이~☆<br>음료랑 같이 먹으면 너도 나도 행복 두배~~</h1>
+		<?php
+			function PrintMenuContent($src, $name) {
+				echo ("<div class='drink-menu'>
+						<img src={$src} alt={$name} class='drink-menu-img'/>
+						<div class='menu-name'>$name</div>
+					</div>");
+			}
+		?>
         <div id="normal-menu-wrapper">
-			<div class="drink-menu">
-				<img src="/ODW/ODW-Private/menus/cookie-black_sesame_smore.jpg" alt="01" class="drink-menu-img"/>
-			</div>
-			<div class="drink-menu">
-				<img src="/ODW/ODW-Private/menus/cookie-choco_levain.jpg" alt="02" class="drink-menu-img"/>
-			</div>
-			<div class="drink-menu">
-				<img src="/ODW/ODW-Private/menus/cookie-marchasmore.jpg" alt="03" class="drink-menu-img"/>
-			</div>
-			<div class="drink-menu">
-				<img src="/ODW/ODW-Private/menus/cookie-matcha_levain.jpg" alt="04" class="drink-menu-img"/>
-			</div>
-			<div class="drink-menu">
-				<img src="/ODW/ODW-Private/menus/cookie-mocha_levain.jpg" alt="04" class="drink-menu-img"/>
-			</div>
-			<div class="drink-menu">
-				<img src="/ODW/ODW-Private/menus/cookie-oreos_more.jpg" alt="04" class="drink-menu-img"/>
-			</div>
-			<div class="drink-menu">
-				<img src="/ODW/ODW-Private/menus/cookie-raisin_scon.jpg" alt="04" class="drink-menu-img"/>
-			</div>
-			<div class="drink-menu">
-				<img src="/ODW/ODW-Private/menus/cookie-walnut_scon.jpg" alt="04" class="drink-menu-img"/>
-			</div>
+        	<?php PrintMenuContent("/ODW/ODW-Private/menus/cookie-black_sesame_smore.jpg", "흑 세사미 스모어"); ?>
+        	<?php PrintMenuContent("/ODW/ODW-Private/menus/cookie-choco_levain.jpg", "초코르뱅"); ?>
+        	<?php PrintMenuContent("/ODW/ODW-Private/menus/cookie-matcha_smore.jpg", "말차 스모어"); ?>
+        	<?php PrintMenuContent("/ODW/ODW-Private/menus/cookie-matcha_levain.jpg", "말차르뱅"); ?>
+        	<?php PrintMenuContent("/ODW/ODW-Private/menus/cookie-mocha_levain.jpg", "모카르뱅"); ?>
+        	<?php PrintMenuContent("/ODW/ODW-Private/menus/cookie-oreo_smore.jpg", "오레오 스모어"); ?>
+        	<?php PrintMenuContent("/ODW/ODW-Private/menus/cookie-raisin_scon.jpg", "레진 스콘"); ?>
+        	<?php PrintMenuContent("/ODW/ODW-Private/menus/cookie-walnut_scon.jpg", "월넛 스콘"); ?>
 		</div>
 		<footer></footer>
     </section>
