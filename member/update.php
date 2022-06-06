5e852dcb3b1c7a4725ecc09a6eb73af856c5e35c
Lee Hak Jun <harleybird23@gmail.com>
2022-06-07 07:48:27 +0900

    design & bugfixed

--- member/update.php
+++ member/update.php
@@ -11,10 +11,11 @@
 <body>
 	<header>
         <?php include($_SERVER['DOCUMENT_ROOT']."/ODW/header.php"); ?>
-		
 	</header> 
     <?php
-        session_start();
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
         require_once($_SERVER['DOCUMENT_ROOT']."/ODW/db/db.php");
         
         $sql = $db -> prepare("SELECT * FROM register WHERE userid=:userid");
@@ -22,10 +23,17 @@
         $sql -> execute();
         $row = $sql -> fetch();
     ?>
-    <section>
+    <section id="normal-section">
+        <div class="sub-menu-bg relative">
+            <!-- 서브메뉴배경 래퍼, 존재이유 : overflow:hidden;, 평소 높이 : 0, 활성화 높이 : 470px -->
+            <div>
+                <!-- 서브메뉴배경, 높이 : 항상 470px, 평소 transform:translateY(-100%), 활성화 transform:translateY(0) -->
+                <div></div>
+            </div>
+        </div>
         <div class="mainCon">
             <div class="updateTitle">회원정보</div>
-            <form action="member_process.php?mode=update" method="post">
+            <form action="member_process.php?mode=update" method="post" class="updateForm">
                 <input type="hidden" name="userid" value="<?= $row['userid']?>">
                 <table class="updateTable">
                     <tr>
@@ -50,8 +58,8 @@
                     </tr>
                 </table>
                 <div class="updateBtn">
-                <input type="submit" value="수정">
-                <input type="button" value="취소" onclick="history.back(1)">
+                    <input type="submit" value="수정">
+                    <input type="button" value="취소" onclick="history.back(1)">
                 </div>
             </form>
         </div>
